7b1a0f029cef64815b0d65f24f5e16643f258fd5
Mehmet <mehmetkusken@gmail.com>
2021-09-05 17:41:22 -0600

    made fetch for theater table

--- src/components/Theater.js
+++ src/components/Theater.js
@@ -1,7 +1,40 @@
 class Theater{
 
+    static all = []
+
+    constructor(info){
+        this.info = info
+        this.constructor.all.push(this)
+    }
+
+    renderTheaterCard = () => {
+        const { id, name, adress, city, state, phone, image } = this.info
+       document.getElementById("theater-container").innerHTML += `
+       <div class="theater-card">
+       <img src=${image} alt=${name}/>
+       <p class="title">${name}</p>
+       <p class="title">${adress}</p>
+       <p class="title">${city}</p>
+       <p class="title">${state}</p>
+       <p class="title">${phone}</p>
+       </div>`
+    }
+
+    static renderTheaterIndex(){
+        const theaterContainer = document.createElement("div")
+        theaterContainer.id = "theater-container"
+        document.getElementById("theater").appendChild(theaterContainer)
+        this.all.forEach(theater => theater.renderTheaterCard())
+    }
+
+    static getTheaters(){
+        api.getTheaters().then(theaters => {
+        theaters.forEach(theater => new Theater(theater))
+        this.renderTheaterIndex()
+        })
+    }
 
 
 
-    
+
 }
